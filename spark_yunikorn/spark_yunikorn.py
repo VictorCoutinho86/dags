8c9ba5c52a53040b67da4f3551c626a763bc45fa
Victor <victor.melo@gazeus.com>
2025-09-26 20:42:30 -0300

    feat: create new dag spark_yunikorn

---
+++ spark_yunikorn/spark_yunikorn.py
@@ -0,0 +1,31 @@
+import os
+from datetime import datetime, timedelta
+from pathlib import Path
+
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import (
+    SparkKubernetesOperator,
+)
+
+with DAG(
+    "spark_pi_yunikorn",
+    default_args={
+        "owner": "airflow",
+        "retries": 1,
+        "retry_delay": timedelta(minutes=5),
+    },
+    description="Submissão da SparkApplication Pi usando Spark Operator no YuniKorn",
+    schedule=None,
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    template_searchpath=str(Path(__file__).parent.resolve()),
+    tags=["spark", "yunikorn"],
+    ) as dag:
+    submit_spark_pi = SparkKubernetesOperator(
+        task_id="submit_spark_pi",
+        namespace="spark",
+        application_file="pi-on-yunikorn.yaml",
+        do_xcom_push=True,
+    )
+
+    submit_spark_pi
