de144d3f5a3e0e642680385902cca62e28896009
Victor Coutinho <victormelo@id.uff.br>
2025-11-07 19:54:24 -0300

    Fix indentation in spark_yunikorn.py

--- spark_yunikorn/spark_yunikorn.py
+++ spark_yunikorn/spark_yunikorn.py
@@ -26,8 +26,7 @@ with DAG(
         namespace="spark-operator",
         application_file="pi-on-yunikorn.yaml",
         do_xcom_push=False,
-        get_logs=True,
-        api_group="sparkoperator.k8s.io"
+        get_logs=True
     )
 
     submit_spark_pi
